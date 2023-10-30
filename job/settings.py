eb61e6b320a717930f45eaf8102ff62fb7363126
Manfre <deividmanfre@gmail.com>
2023-10-30 09:28:45 -0300

    adjust the project

--- job/settings.py
+++ job/settings.py
@@ -79,11 +79,14 @@ WSGI_APPLICATION = 'job.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mydb',
+        'USER': 'POSTGRES_USER',
+        'PASSWORD': '123456',
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
